2da76ded09d919419f6835913f88e136b63658de
assil kherfi <kherfi.assil@gmail.com>
2023-08-31 12:09:30 +0100

    test

--- Retention_dashboard.py
+++ Retention_dashboard.py
@@ -4,33 +4,51 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from operator import attrgetter
-from st_files_connection import FilesConnection
 from datetime import datetime, timedelta
 import os
+import boto3
+from io import StringIO
 
 # %%
-# Récupération des secrets pour la base de données
-db_username = st.secrets["db_username"]
-db_password = st.secrets["db_password"]
-# Récupération des secrets pour S3
-s3_access_key = st.secrets["AWS_S3_credentials"]["access_key"]
-s3_secret_key = st.secrets["AWS_S3_credentials"]["secret_key"]
+# Accéder aux secrets de la section "s3_credentials"
+s3_secrets = st.secrets["s3_credentials"]
 
-# Create connection object and retrieve file contents.
-# Specify input format is a csv and to cache the result for 600 seconds.
-conn = st.experimental_connection("s3", type=FilesConnection)
-orders = conn.read("one-data-lake/orders.csv", input_format="csv", ttl=600)
-
-order_details = conn.read(
-    "one-data-lake/order_details.csv", input_format="csv", ttl=600
+# Créer une session AWS
+session = boto3.Session(
+    aws_access_key_id=s3_secrets["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key=s3_secrets["AWS_SECRET_ACCESS_KEY"],
 )
-users = conn.read("one-data-lake/", input_format="users.csv", ttl=600)
-external_pmi = conn.read("one-data-lake/", input_format="pmi_external.csv", ttl=600)
 
+# Créer un client S3
+s3_client = session.client("s3")
 
-# Print results.
-for row in orders.itertuples():
-    st.write(f"{row.Owner} has a :{row.Pet}:")
+# Nom du seau S3
+bucket_name = "one-data-lake"
+
+# Liste des noms de fichiers à télécharger et à traiter
+file_names = [
+    "csv_database/orders.csv",
+    "csv_database/users.csv",
+    # "csv_database/order_details.csv",
+    # "csv_database/pmi_external.csv"
+]
+
+# Dictionnaire pour stocker les DataFrames correspondants aux fichiers
+dataframes = {}
+
+# Télécharger et traiter les fichiers
+for file_name in file_names:
+    response = s3_client.get_object(Bucket=bucket_name, Key=file_name)
+    object_content = response["Body"].read().decode("utf-8")
+    df_name = file_name.split("/")[-1].split(".")[0]  # Obtenir le nom du DataFrame
+    dataframes[df_name] = pd.read_csv(StringIO(object_content))
+
+# Créer un DataFrame à partir des données
+orders = dataframes["orders"]
+users = dataframes["users"]
+# order_details = dataframes["order_details"]
+# pmi_external = dataframes["pmi_external"]
+
 
 # %%
 pd.set_option("display.max_columns", None)
@@ -151,142 +169,142 @@ orders = orders[
 orders = orders.rename(columns={"Order Type": "Order_Type"})
 orders.loc[(orders["customer_id"] == "73187559488.0"), "Order_Type"] = "EXTERNE"
 
-orders_pmi = orders[orders["Order_Type"] == "EXTERNE"]
+# orders_pmi = orders[orders["Order_Type"] == "EXTERNE"]
 
-external_pmi = pd.read_csv("pmi_external.csv", delimiter=",", low_memory=False)
-external_pmi["order_id"] = external_pmi["order_id"].astype(str)
-external_pmi["customer_id"] = external_pmi["customer_id"].astype(str)
-external_pmi = external_pmi.rename(columns={"job_status": "Status"})
-external_pmi = external_pmi[~external_pmi["Status"].isin(["ABANDONED"])]
-external_pmi["createdAt"] = pd.to_datetime(external_pmi["createdAt"])
-external_pmi["customer_id"] = external_pmi["customer_id"].str.rstrip(".0")
-external_pmi["businessCat"] = external_pmi["businessCat"].replace(
-    ["Recharge mobile", "Recharge mobile / ADSL"], ["Airtime", "Airtime"]
-)
+# external_pmi = pd.read_csv("pmi_external.csv", delimiter=",", low_memory=False)
+# external_pmi["order_id"] = external_pmi["order_id"].astype(str)
+# external_pmi["customer_id"] = external_pmi["customer_id"].astype(str)
+# external_pmi = external_pmi.rename(columns={"job_status": "Status"})
+# external_pmi = external_pmi[~external_pmi["Status"].isin(["ABANDONED"])]
+# external_pmi["createdAt"] = pd.to_datetime(external_pmi["createdAt"])
+# external_pmi["customer_id"] = external_pmi["customer_id"].str.rstrip(".0")
+# external_pmi["businessCat"] = external_pmi["businessCat"].replace(
+#     ["Recharge mobile", "Recharge mobile / ADSL"], ["Airtime", "Airtime"]
+# )
 
-# order_details = pd.read_csv("order_details.csv", delimiter=",", low_memory=False)
-order_details["order_id"] = order_details["order_id"].astype(str)
-order_details["customer_id"] = order_details["customer_id"].astype(str)
-order_details["createdAt"] = pd.to_datetime(order_details["createdAt"])
-order_details["product_id"] = order_details["product_id"].apply(int)
-order_details = order_details.rename(columns={"job_status": "Status"})
-order_details = order_details[~order_details["Status"].isin(["ABANDONED"])]
-order_details = order_details[
-    ~order_details["order_id"].isin(
-        [
-            "734138951872",
-            "811738356736",
-            "648042957760",
-            "239046556928",
-            "423486580736",
-            "536463465088",
-        ]
-    )
-]
-order_details = order_details[
-    ~order_details["customer_id"].isin(
-        [
-            "2059318.0",
-            "1506025442.0",
-            "1694397201.0",
-            "2830181885.0",
-            "5620828389.0",
-            "4064611739.0",
-            "3385745613.0",
-            "2281370.0",
-            "64438759505.0",
-            "569994573568.0",
-            "1628682.0",
-            "310179181696.0",
-            "878446.0",
-            "3643707.0",
-            "2253354.0",
-            "1771017743.0",
-            "727840660224.0",
-            "2280761953.0",
-            "2864429.0",
-            "1505970032.0",
-            "1517116.0",
-            "929482210496.0",
-            "5884716233.0",
-            "22781605568.0",
-            "2794629.0",
-            "47201675489.0",
-            "6072524763.0",
-            "2342577.0",
-            "1440074.0",
-            "3666483.0",
-            "449701472960.0",
-            "869120.0",
-            "7304625963.0",
-            "2214784702.0",
-            "869883.0",
-            "2851778338.0",
-            "3000794.0",
-            "1898245261.0",
-            "9816298466.0",
-            "7021529167.0",
-            "3017838801.0",
-            "5624710564.0",
-            "1584024035.0",
-            "2485567.0",
-            "2763532338.0",
-            "841024809600.0",
-            "1739473.0",
-            "2183725.0",
-            "3788062.0",
-            "23400912794.0",
-            "150321448192.0",
-            "461317394880.0",
-            "2208215.0",
-            "3669307840.0",
-            "610335616576.0",
-            "7478577450.0",
-            "13153632574.0",
-            "2815691755.0",
-            "879984.0",
-            "3312616.0",
-            "548088380288.0",
-            "3526036.0",
-            "2367635120.0",
-            "24957125457.0",
-            "459557812544.0",
-            "1290757210.0",
-            "507345740736.0",
-            "2558315057.0",
-            "819751.0",
-            "407181581440.0",
-            "1412707541.0",
-            "1419613392.0",
-            "4068655.0",
-            "303655560704.0",
-            "2389210.0",
-            "2765139.0",
-            "504153462208.0",
-            "2100305133.0",
-            "653243920384.0",
-            "1253878877.0",
-            "43255929830.0",
-        ]
-    )
-]
-order_details = order_details.rename(columns={"Order Type": "Order_Type"})
-order_details.loc[
-    (order_details["customer_id"] == "73187559488.0"), "Order_Type"
-] = "EXTERNE"
-order_details["customer_id"] = order_details["customer_id"].str.rstrip(".0")
-order_details["businessCat"] = order_details["businessCat"].replace(
-    ["Recharge mobile", "Recharge mobile / ADSL"], ["Airtime", "Airtime"]
-)
-order_details["customer_origine"] = order_details["paymentType"].apply(
-    lambda x: "Diaspora" if x == "CARD_PAY" else "Local"
-)
-dict_1 = dict(zip(orders["order_id"], orders["transactionId"]))
-dict_2 = dict(zip(orders["order_id"], orders["service_fees"]))
-order_details["transactionId"] = order_details["order_id"].map(dict_1)
-order_details["service_fees"] = order_details["order_id"].map(dict_2)
+# # order_details = pd.read_csv("order_details.csv", delimiter=",", low_memory=False)
+# order_details["order_id"] = order_details["order_id"].astype(str)
+# order_details["customer_id"] = order_details["customer_id"].astype(str)
+# order_details["createdAt"] = pd.to_datetime(order_details["createdAt"])
+# order_details["product_id"] = order_details["product_id"].apply(int)
+# order_details = order_details.rename(columns={"job_status": "Status"})
+# order_details = order_details[~order_details["Status"].isin(["ABANDONED"])]
+# order_details = order_details[
+#     ~order_details["order_id"].isin(
+#         [
+#             "734138951872",
+#             "811738356736",
+#             "648042957760",
+#             "239046556928",
+#             "423486580736",
+#             "536463465088",
+#         ]
+#     )
+# ]
+# order_details = order_details[
+#     ~order_details["customer_id"].isin(
+#         [
+#             "2059318.0",
+#             "1506025442.0",
+#             "1694397201.0",
+#             "2830181885.0",
+#             "5620828389.0",
+#             "4064611739.0",
+#             "3385745613.0",
+#             "2281370.0",
+#             "64438759505.0",
+#             "569994573568.0",
+#             "1628682.0",
+#             "310179181696.0",
+#             "878446.0",
+#             "3643707.0",
+#             "2253354.0",
+#             "1771017743.0",
+#             "727840660224.0",
+#             "2280761953.0",
+#             "2864429.0",
+#             "1505970032.0",
+#             "1517116.0",
+#             "929482210496.0",
+#             "5884716233.0",
+#             "22781605568.0",
+#             "2794629.0",
+#             "47201675489.0",
+#             "6072524763.0",
+#             "2342577.0",
+#             "1440074.0",
+#             "3666483.0",
+#             "449701472960.0",
+#             "869120.0",
+#             "7304625963.0",
+#             "2214784702.0",
+#             "869883.0",
+#             "2851778338.0",
+#             "3000794.0",
+#             "1898245261.0",
+#             "9816298466.0",
+#             "7021529167.0",
+#             "3017838801.0",
+#             "5624710564.0",
+#             "1584024035.0",
+#             "2485567.0",
+#             "2763532338.0",
+#             "841024809600.0",
+#             "1739473.0",
+#             "2183725.0",
+#             "3788062.0",
+#             "23400912794.0",
+#             "150321448192.0",
+#             "461317394880.0",
+#             "2208215.0",
+#             "3669307840.0",
+#             "610335616576.0",
+#             "7478577450.0",
+#             "13153632574.0",
+#             "2815691755.0",
+#             "879984.0",
+#             "3312616.0",
+#             "548088380288.0",
+#             "3526036.0",
+#             "2367635120.0",
+#             "24957125457.0",
+#             "459557812544.0",
+#             "1290757210.0",
+#             "507345740736.0",
+#             "2558315057.0",
+#             "819751.0",
+#             "407181581440.0",
+#             "1412707541.0",
+#             "1419613392.0",
+#             "4068655.0",
+#             "303655560704.0",
+#             "2389210.0",
+#             "2765139.0",
+#             "504153462208.0",
+#             "2100305133.0",
+#             "653243920384.0",
+#             "1253878877.0",
+#             "43255929830.0",
+#         ]
+#     )
+# ]
+# order_details = order_details.rename(columns={"Order Type": "Order_Type"})
+# order_details.loc[
+#     (order_details["customer_id"] == "73187559488.0"), "Order_Type"
+# ] = "EXTERNE"
+# order_details["customer_id"] = order_details["customer_id"].str.rstrip(".0")
+# order_details["businessCat"] = order_details["businessCat"].replace(
+#     ["Recharge mobile", "Recharge mobile / ADSL"], ["Airtime", "Airtime"]
+# )
+# order_details["customer_origine"] = order_details["paymentType"].apply(
+#     lambda x: "Diaspora" if x == "CARD_PAY" else "Local"
+# )
+# dict_1 = dict(zip(orders["order_id"], orders["transactionId"]))
+# dict_2 = dict(zip(orders["order_id"], orders["service_fees"]))
+# order_details["transactionId"] = order_details["order_id"].map(dict_1)
+# order_details["service_fees"] = order_details["order_id"].map(dict_2)
 
-order_details_pmi = order_details[order_details["Order_Type"] == "EXTERNE"]
+# order_details_pmi = order_details[order_details["Order_Type"] == "EXTERNE"]
 
 # users = pd.read_csv("users.csv", delimiter=",", low_memory=False)
 users["customer_id"] = users["customer_id"].astype(str)
