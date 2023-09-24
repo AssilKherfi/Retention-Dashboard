8539e3508e380664cf2a29a3179ec73c2ea1a4c7
assil kherfi <kherfi.assil@gmail.com>
2023-09-24 17:32:37 +0100

    change the date filter

--- Retention_dashboard.py
+++ Retention_dashboard.py
@@ -599,102 +599,102 @@ def main():
         retention.index = retention.index.strftime("%Y-%m")
         retention.columns = retention.columns.astype(str)
 
-        # Créez une fonction pour formater les valeurs
-        def format_value(x):
-            if not pd.isna(x):
-                return f"{x:.2f}"
-            else:
-                return ""
+        # # Créez une fonction pour formater les valeurs
+        # def format_value(x):
+        #     if not pd.isna(x):
+        #         return f"{x:.2f}"
+        #     else:
+        #         return ""
 
-        # Appliquez la fonction pour formater les valeurs dans le DataFrame
-        heatmap_data = retention * 100
-        heatmap_data = heatmap_data.applymap(format_value)
+        # # Appliquez la fonction pour formater les valeurs dans le DataFrame
+        # heatmap_data = retention * 100
+        # heatmap_data = heatmap_data.applymap(format_value)
 
-        # Créez une liste des étiquettes d'axe X (period_number) et d'axe Y (cohort)
-        x_labels = heatmap_data.columns.tolist()  # Liste des périodes (0, 1, 2, ...)
-        y_labels = (
-            heatmap_data.index.tolist()
-        )  # Liste des cohortes (2023-01, 2023-02, ...)
+        # # Créez une liste des étiquettes d'axe X (period_number) et d'axe Y (cohort)
+        # x_labels = heatmap_data.columns.tolist()  # Liste des périodes (0, 1, 2, ...)
+        # y_labels = (
+        #     heatmap_data.index.tolist()
+        # )  # Liste des cohortes (2023-01, 2023-02, ...)
 
-        # Créez un graphique en utilisant px.imshow avec les étiquettes X et Y spécifiées
-        fig_retention = px.imshow(heatmap_data, x=x_labels, y=y_labels)
+        # # Créez un graphique en utilisant px.imshow avec les étiquettes X et Y spécifiées
+        # fig_retention = px.imshow(heatmap_data, x=x_labels, y=y_labels)
 
-        # Personnalisez le texte à afficher pour chaque point de données (gardez deux chiffres après la virgule)
-        custom_data = [
-            [f"{value:.2f}%" if value is not None else "" for value in row]
-            for row in (retention * 100).values
-        ]
+        # # Personnalisez le texte à afficher pour chaque point de données (gardez deux chiffres après la virgule)
+        # custom_data = [
+        #     [f"{value:.2f}%" if value is not None else "" for value in row]
+        #     for row in (retention * 100).values
+        # ]
 
-        # Mettez à jour le texte personnalisé dans le graphique
-        fig_retention.update_traces(
-            customdata=custom_data, hovertemplate="%{customdata}<extra></extra>"
-        )
+        # # Mettez à jour le texte personnalisé dans le graphique
+        # fig_retention.update_traces(
+        #     customdata=custom_data, hovertemplate="%{customdata}<extra></extra>"
+        # )
 
-        # Ajoutez les annotations dans les cases de la heatmap
-        for i in range(len(y_labels)):
-            for j in range(len(x_labels)):
-                value = heatmap_data.iloc[i, j]
-                if not pd.isna(value):
-                    font_color = (
-                        "black" if j == 0 else "white"
-                    )  # Noir pour la première colonne, blanc pour les autres
-                    fig_retention.add_annotation(
-                        text=value,  # Format du texte à afficher
-                        x=x_labels[j],
-                        y=y_labels[i],
-                        showarrow=False,
-                        font=dict(color=font_color),  # Couleur du texte
-                    )
+        # # Ajoutez les annotations dans les cases de la heatmap
+        # for i in range(len(y_labels)):
+        #     for j in range(len(x_labels)):
+        #         value = heatmap_data.iloc[i, j]
+        #         if not pd.isna(value):
+        #             font_color = (
+        #                 "black" if j == 0 else "white"
+        #             )  # Noir pour la première colonne, blanc pour les autres
+        #             fig_retention.add_annotation(
+        #                 text=value,  # Format du texte à afficher
+        #                 x=x_labels[j],
+        #                 y=y_labels[i],
+        #                 showarrow=False,
+        #                 font=dict(color=font_color),  # Couleur du texte
+        #             )
 
-        # Créez la heatmap de la matrice de Retention analysis en pourcentage
-        cohort_pivot.index = cohort_pivot.index.strftime("%Y-%m")
-        cohort_pivot.columns = cohort_pivot.columns.astype(str)
+        # # Créez la heatmap de la matrice de Retention analysis en pourcentage
+        # cohort_pivot.index = cohort_pivot.index.strftime("%Y-%m")
+        # cohort_pivot.columns = cohort_pivot.columns.astype(str)
 
-        # Créez une liste des étiquettes d'axe X (period_number) et d'axe Y (cohort)
-        x_labels = cohort_pivot.columns.tolist()  # Liste des périodes (0, 1, 2, ...)
-        y_labels = (
-            cohort_pivot.index.tolist()
-        )  # Liste des cohortes (2023-01, 2023-02, ...)
+        # # Créez une liste des étiquettes d'axe X (period_number) et d'axe Y (cohort)
+        # x_labels = cohort_pivot.columns.tolist()  # Liste des périodes (0, 1, 2, ...)
+        # y_labels = (
+        #     cohort_pivot.index.tolist()
+        # )  # Liste des cohortes (2023-01, 2023-02, ...)
 
-        # Créez un graphique en utilisant px.imshow avec les étiquettes X et Y spécifiées
-        fig_clients = px.imshow(cohort_pivot, x=x_labels, y=y_labels)
+        # # Créez un graphique en utilisant px.imshow avec les étiquettes X et Y spécifiées
+        # fig_clients = px.imshow(cohort_pivot, x=x_labels, y=y_labels)
 
-        # Ajoutez les annotations dans les cases de la heatmap
-        for i in range(len(y_labels)):
-            for j in range(len(x_labels)):
-                value = cohort_pivot.iloc[i, j]
-                if not pd.isna(value):
-                    font_color = (
-                        "black" if j == 0 else "white"
-                    )  # Noir pour la première colonne, blanc pour les autres
-                    fig_clients.add_annotation(
-                        text=value,  # Format du texte à afficher
-                        x=x_labels[j],
-                        y=y_labels[i],
-                        showarrow=False,
-                        font=dict(color=font_color),  # Couleur du texte
-                    )
+        # # Ajoutez les annotations dans les cases de la heatmap
+        # for i in range(len(y_labels)):
+        #     for j in range(len(x_labels)):
+        #         value = cohort_pivot.iloc[i, j]
+        #         if not pd.isna(value):
+        #             font_color = (
+        #                 "black" if j == 0 else "white"
+        #             )  # Noir pour la première colonne, blanc pour les autres
+        #             fig_clients.add_annotation(
+        #                 text=value,  # Format du texte à afficher
+        #                 x=x_labels[j],
+        #                 y=y_labels[i],
+        #                 showarrow=False,
+        #                 font=dict(color=font_color),  # Couleur du texte
+        #             )
 
-        # Créez des onglets pour basculer entre les deux visualisations
-        selected_visualization = st.radio(
-            "Sélectionnez la visualisation", ["Retention Analysis", "Nombre de Clients"]
-        )
+        # # Créez des onglets pour basculer entre les deux visualisations
+        # selected_visualization = st.radio(
+        #     "Sélectionnez la visualisation", ["Retention Analysis", "Nombre de Clients"]
+        # )
 
-        if selected_visualization == "Retention Analysis":
-            # Affichez la heatmap de l'analyse de rétention
-            st.plotly_chart(fig_retention)  # Utilisez le graphique fig_retention
-        else:
-            # Affichez la heatmap du nombre de clients
-            st.plotly_chart(fig_clients)  # Utilisez le graphique fig_clients
+        # if selected_visualization == "Retention Analysis":
+        #     # Affichez la heatmap de l'analyse de rétention
+        #     st.plotly_chart(fig_retention)  # Utilisez le graphique fig_retention
+        # else:
+        #     # Affichez la heatmap du nombre de clients
+        #     st.plotly_chart(fig_clients)  # Utilisez le graphique fig_clients
 
-        # Téléchargement de la  Rétention
-        retention_analysis_xlsx = to_excel(retention, include_index=True)
-        st.download_button(
-            "Télécharger la Retention analysis en Excel (.xlsx)",
-            retention_analysis_xlsx,
-            f"Retention analysis - ORIGINE : {customer_origine} - BUSINESS CATÈGORIE : {business_cat} - STATUS : {status}, pour les {num_periods} derniers {time_period}.xlsx",
-            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        )
+        # # Téléchargement de la  Rétention
+        # retention_analysis_xlsx = to_excel(retention, include_index=True)
+        # st.download_button(
+        #     "Télécharger la Retention analysis en Excel (.xlsx)",
+        #     retention_analysis_xlsx,
+        #     f"Retention analysis - ORIGINE : {customer_origine} - BUSINESS CATÈGORIE : {business_cat} - STATUS : {status}, pour les {num_periods} derniers {time_period}.xlsx",
+        #     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        # )
 
         # Téléchargement de la data de Client cohort en excel
         cohort_pivot_xlsx = to_excel(cohort_pivot, include_index=True)
